9eac0290ff765d69c586a1e9ed307af874d3a8bc
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-12 11:48:58 +0700

    add exercise16

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -10,27 +10,6 @@ namespace Exercise
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(SumPrime( new List<int> { 2, 3, 4, 5, 6, 7, 8, 9 }));
-            Console.ReadLine();
-        }
-        public static int SumPrime(List<int> numbers)
-        {
-            return numbers.Where(num => isPrime(num)).Sum();
-        }
-        public static bool isPrime(int number)
-        {
-            if (number < 2)
-            {
-                return false;
-            }
-            for (int i = 2; i <= Math.Sqrt(number); i++)
-            {
-                if (number % i == 0)
-                {
-                    return false;
-                }
-            }
-            return true;
         }
     }
 }

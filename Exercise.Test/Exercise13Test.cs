e8ccf6128a1f1750d2651f3856cf62e7e836ca48
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-12 10:24:49 +0700

    add exercise13

---
+++ Exercise.Test/Exercise13Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise13Test
+    {
+        [TestMethod]
+        public void TestSubtract()
+        {
+            Assert.AreEqual(new Exercise13().Subtract(new List<int> { 1,2,3,4,5,6,7,8,9}),-5);
+            Assert.AreEqual(new Exercise13().Subtract(new List<int> {0,1,13,25,68,100}),129);
+        }
+    }
+}

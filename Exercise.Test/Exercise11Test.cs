febebde6d6a0abfa96efc10f3d313c8390a12079
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-12 09:36:31 +0700

    add-exercise11

---
+++ Exercise.Test/Exercise11Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise11Test
+    {
+        [TestMethod]
+        public void TestDuplicateElement()
+        {
+            Assert.AreEqual(new Exercise11().DuplicateElement(new int[6] { 1,2,3,4,5,6}), "");
+            Assert.AreEqual(new Exercise11().DuplicateElement(new int[6] { 2, 2, 3, 4, 5, 5 }), "2 5");
+
+        }
+    }
+}
--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -10,7 +10,6 @@ namespace Exercise
     {
         static void Main(string[] args)
         {
-           
         }
     }
 }

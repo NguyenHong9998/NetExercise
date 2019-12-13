a99157caa1c9be33a1cc1980546e68a9b0934020
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-14 00:09:57 +0700

    add exercise24

---
+++ Exercise.Test/Exercise24Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise24Test
+    {
+        [TestMethod]
+        public void TestSumOfNumbersInString()
+        {
+            Assert.AreEqual(new Exercise24().SumOfNumbersInString("abc 123 def 33 mn 3.221"), 380);
+            Assert.AreEqual(new Exercise24().SumOfNumbersInString("123Hong3Cute"), 126);
+        }
+    }
+}
---
+++ Exercise/Exercise24.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+namespace Exercise
+{
+    public class Exercise24
+    {
+        public int SumOfNumbersInString(string stringOfCharacters)
+        {
+            string[] items = Regex.Split(stringOfCharacters, @"\D+");
+            int sum = 0;
+            foreach (string item in items)
+            {
+                if (item != "")
+                {
+                    sum += int.Parse(item);
+                }
+            }
+            return sum;
+        }
+    }
+}

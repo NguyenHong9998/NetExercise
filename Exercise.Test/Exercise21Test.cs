ef3d4e486d08d5a541ed55330360bb310ab169b4
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-13 22:33:10 +0700

    add exercise21

---
+++ Exercise.Test/Exercise21Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise21Test
+    {
+        [TestMethod]
+        public void TestToLowerCase()
+        {
+            Assert.AreEqual(new Exercise21().ToLowerCase("ABC_abc"), "abc_abc");
+            Assert.AreEqual(new Exercise21().ToLowerCase("Nguyen Thi Hong"), "nguyen thi hong");
+        }
+    }
+}

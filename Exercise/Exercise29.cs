6d5ee80768fb5777343139af2d257915abf34a7d
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-16 16:45:34 +0700

    add exercise29

---
+++ Exercise/Exercise29.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise29
+    {
+        public bool isBarcodeEAN(string numbers)
+        {
+            int sum = 0;
+            for(int i = 0; i < numbers.Length; i++)
+            {
+                if ((i + 1) % 2 == 0)
+                {
+                    sum += 3*numbers[i];
+                }
+                else
+                {
+                    sum += numbers[i];
+                }
+            }
+            return sum % 10 == 0;
+        }
+    }
+}

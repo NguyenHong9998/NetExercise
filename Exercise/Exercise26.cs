b1008057a1547ac39c307fc5dde698a922b046ab
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-16 16:01:45 +0700

    add exercise26

---
+++ Exercise/Exercise26.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise26
+    {
+        public string CompactString(string stringOfCharacters)
+        {
+            List<char> letters = stringOfCharacters.ToCharArray().ToList();
+            string result = "";
+            var temp = letters.GroupBy(x => x);
+            foreach (IGrouping<char, char> itemGroup in temp)
+            {
+                result += itemGroup.Key ;
+                if (itemGroup.Count() != 1)
+                {
+                    result += itemGroup.Count().ToString();
+                }
+            }
+            return result;
+        }
+    }
+}

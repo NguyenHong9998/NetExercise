0f15c9733c00e9d7ff282a6ccad485eaacdc07dc
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-16 15:05:31 +0700

    add exercise25

---
+++ Exercise.Test/Exercise25Test.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise25Test
+    {
+        [TestMethod]
+        public void TestIsSymmetry()
+        {
+            Assert.IsTrue(new Exercise25().IsSymmetry("abccba"));
+            Assert.IsTrue(new Exercise25().IsSymmetry("123454321"));
+        }
+    }
+}
---
+++ Exercise/Exercise25.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise25
+    {
+        public bool IsSymmetry(string stringOfCharacters)
+        {
+            char[] letters = stringOfCharacters.ToCharArray();
+            Array.Reverse(letters);
+            string s = new string(letters);
+            return stringOfCharacters.Equals(s);
+        }
+    }
+}

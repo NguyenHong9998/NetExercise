89868e1585ecb2e102c01b8b3f1797f7e842c6fd
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-17 22:35:16 +0700

    error 8-11

---
+++ Exercise.Test/Exercise08Test.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise08Test
+    {
+        [TestMethod]
+        public void TestPi()
+        {
+            Assert.IsTrue(new Exercise08().Pi() - Math.PI <= 0.001);
+        }
+    }
+}

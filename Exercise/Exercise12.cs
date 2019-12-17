cebd6d6998d9c69cf512b00474bfe9ce6f45687f
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-17 22:46:19 +0700

    add exercise12

---
+++ Exercise/Exercise12.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise12
+    {
+        public int SumOfThreeLargestNumbers(int[] numbers)
+        {
+            List<int> list = numbers.ToList();
+            list.Sort();
+            list.Reverse();
+            return list.ElementAt(0) + list.ElementAt(1) + list.ElementAt(2);
+        }
+    }
+}

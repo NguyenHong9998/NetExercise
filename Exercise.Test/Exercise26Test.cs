04fa7fafa7c4a9f36785aa837a1c45679735789a
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-18 15:46:09 +0700

    Add -exercise 26

---
+++ Exercise.Test/Exercise26Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise26Test
+    {
+        [TestMethod]
+        public void TestCompactString()
+        {
+            Assert.AreEqual(new Exercise26().CompactString("abbbbbbbbbbbbbc"), "ab13c");
+            Assert.AreEqual(new Exercise26().CompactString("abcccceeeeeefd"), "abc4e6fd");
+            Assert.AreEqual(new Exercise26().CompactString(""), "");
+        }
+    }
+}
---
+++ Exercise/Exercise26.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise26
+    {
+        public string CompactString(string stringOfCharacters)
+        {
+            var result = "";
+            var temp = stringOfCharacters.GroupBy(x => x);
+            foreach (var itemGroup in temp)
+            {
+                result += itemGroup.Key ;
+                if (itemGroup.Count() != 1)
+                {
+                    result += itemGroup.Count();
+                }
+            }
+            return result;
+        }
+    }
+}

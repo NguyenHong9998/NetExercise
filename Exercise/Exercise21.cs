53911027ac6644750c16fcb4e84a3bcdd5ff97d1
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-18 15:19:21 +0700

    Add exercise 21

---
+++ Exercise/Exercise21.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise21
+    {
+        public string ToLowerCase(string stringOfCharacters)
+        {
+            return stringOfCharacters.ToLower();
+        }
+    }
+}

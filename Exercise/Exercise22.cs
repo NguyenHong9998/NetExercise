56a81d22c6879ea7a717c679a02e4ab84a370127
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-18 15:22:14 +0700

    Add exercise 22

---
+++ Exercise/Exercise22.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise22
+    {
+        public string ToUpperCase(string stringOfCharacters)
+        {
+            return stringOfCharacters.ToUpper();
+        }
+    }
+}

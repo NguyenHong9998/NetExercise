f674c464f0bf218611ae2ae11ff14df8a034ae25
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-18 15:08:47 +0700

    Add exercise 18

---
+++ Exercise/Exercise18.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise18
+    {
+        public int CountNumber(int[] number)
+        {
+            return number.ToList().Where(x => (x % 3 == 0 && x % 5 != 0)).Count();
+        }
+    }
+}

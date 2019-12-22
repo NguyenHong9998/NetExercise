00a844d284c7d0af209521f68beccbe9a4064dfd
NguyenHong9998 <nguyenhong19999@gmail.com>
2019-12-22 22:08:38 +0700

    Add exercise 28

---
+++ Exercise.Test/Exercise28Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise.Test
+{
+    [TestClass]
+    public class Exercise28Test
+    {
+        [TestMethod]
+        public void TestTranslateString()
+        {
+            Assert.AreEqual(new Exercise28().TranslateString("ab13c"), "abbbbbbbbbbbbbc");
+            Assert.AreEqual(new Exercise28().TranslateString("abc4e6fd"), "abcccceeeeeefd");
+            Assert.AreEqual(new Exercise28().TranslateString(""), "");
+        }
+    }
+}
---
+++ Exercise/Exercise28.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise
+{
+    public class Exercise28
+    {
+        public string TranslateString(string stringOfCharacters)
+        {
+            var letters = stringOfCharacters.ToCharArray();
+            var result = "";
+            if (string.IsNullOrEmpty(stringOfCharacters))
+            {
+                return result;
+            }
+            var letter="" ;
+            int start = 0;
+            int end = 0;
+            int num = 0;
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (!char.IsDigit(letters[i]))
+                {
+                    result += letters[i];
+                    letter = letters[i].ToString();
+                }
+                else
+                {
+                    start = i;
+                    end = i;
+                    while (char.IsDigit(letters[end]))
+                    {
+                        end++;
+                    }
+                    num = int.Parse(stringOfCharacters.Substring(start, end - start));
+                    for (int j = 0; j < num - 1; j++)
+                    {
+                        result += letter;
+                    }
+                    i = end - 1;
+                }
+            }
+            return result;
+        }
+    }
+}
+
